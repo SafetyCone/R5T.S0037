9a68ed3ccf3eee0603b98f8888a6e1cddbf39d5f
davidcoats <david@getsafetycone.com>
2022-06-09 12:34:51 -0700

    Interim changes.

--- source/R5T.S0037/Code/Program.cs
+++ source/R5T.S0037/Code/Program.cs
@@ -1,59 +1,18 @@
 using System;
-using System.Threading;
 using System.Threading.Tasks;
-
-using Microsoft.Extensions.Hosting;
-
-using R5T.Plymouth;
-using R5T.Plymouth.ProgramAsAService;
 
 
 namespace R5T.S0037
 {
-    class Program : ProgramAsAServiceBase
+    class Program
     {
-        #region Static
-        
         static Task Main()
         {
-        
-            return ApplicationBuilder.Instance
-                .NewApplication()
-                .UseProgramAsAService<Program>()
-                .UseT0027_T009_TwoStageStartup<Startup>()
-                .BuildProgramAsAServiceHost()
-                .Run();
+            Console.WriteLine("A");
+
+            return Task.CompletedTask;
         }
 
-        #endregion
-
-
-                        private IServiceProvider ServiceProvider { get; }
-        
-
-
-                        public Program(IApplicationLifetime applicationLifetime,
-            IServiceProvider serviceProvider)
-            : base(applicationLifetime)
-        {
-        
-            this.ServiceProvider = serviceProvider;
-        }
-
-                protected override Task ServiceMain(CancellationToken stoppingToken)
-        {
-        
-            return this.RunOperation();
-        }
-
-                private async Task RunOperation()
-        {
-        
-        }
-
-                private async Task RunMethod()
-        {
-        
-        }
+        static void Create
     }
 }
